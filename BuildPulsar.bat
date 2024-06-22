0e899e96d39e25a5bdea5dd5717e0fe691903379
MelgMKW <melgmkw@gmail.com>
2024-06-22 22:30:36 +0200

    Correct BuildPulsar.bat

--- BuildPulsar.bat
+++ BuildPulsar.bat
@@ -4,29 +4,36 @@ SETLOCAL EnableDelayedExpansion
 cls
 del build\*.o
 
+::DEBUG only works if you have the map and readelf (which is part of MinGW)
 SET "debug="
-SET "cwDWARF="
-if "%1" equ "-d" SET "debug=-debug -map=^"C:\Users\admin\Documents\Dolphin Emulator\Maps\RMCP01.map^" -readelf=^"C:\MinGW\bin\readelf.exe^""
-if "%1" equ "-d" SET "cwDWARF=-g"
+::SET "cwDWARF="
+::if "%1" equ "-d" SET "debug=-debug -map=^"Dolphin Emulator\Maps\RMCP01.map^" -readelf=^"C:\MinGW\bin\readelf.exe^""
+::if "%1" equ "-d" SET "cwDWARF=-g"
 
+:: Sources and Compiler
+SET "ENGINE=.\KamekInclude"
+set "GAMESOURCE=.\GameSource"
+SET "PULSAR=.\PulsarEngine"
 
-:: Destination (change as necessary)
-SET "SOURCE=Pulsar"
+:: Change this as necessary depending on where you put CodeWarrior
+SET "CC="
+
+:: Riivolution Destination (change as necessary)
 SET "RIIVO="
-SET "ENGINE=C:\Modding\Coding\Kamek\Common\KamekInclude"
-SET "CREATOR=C:\Modding\Coding\Kamek\Pulsar\PulsarPackCreator\Resources"
-echo %RIIVO%
 
-
-:: CPP compilation settings
-SET CC="../Common/cw/mwcceppc.exe"
-SET CFLAGS=-I- -i "../Common/KamekInclude" -i "../Common/GameSource" -i "../Common/GameSource/MarioKartWii" -i PulsarEngine ^
+:: Compiler flags and folder
+SET CFLAGS=-I- -i %ENGINE% -i %GAMESOURCE% -i %PULSAR% ^
   -opt all -inline auto -enum int -proc gekko -fp hard -sdata 0 -sdata2 0 -maxerrors 1 -func_align 4 %cwDWARF%
 SET DEFINE=
 
+if "!CC!" == "" (
+    echo You need to specify the path to CodeWarrior by setting the CC variable
+    exit /b 1
+)
+
 :: CPP Sources
 SET CPPFILES=
-for /R PulsarEngine %%f in (*.cpp) do SET "CPPFILES=%%f !CPPFILES!"
+for /R %PULSAR% %%f in (*.cpp) do SET "CPPFILES=%%f !CPPFILES!"
 
 :: Compile CPP
 %CC% %CFLAGS% -c -o "build/kamek.o" "%ENGINE%\kamek.cpp"
@@ -40,11 +47,10 @@ FOR %%H IN (%CPPFILES%) DO (
 
 :: Link
 echo Linking... %time%
-"../Common/Kamek" "build/kamek.o" %OBJECTS% %debug% -dynamic -externals="../Common/GameSource/symbols.txt" -versions="../Common/GameSource/versions.txt" -output-combined=build\Code.pul
+".\KamekLinker\Kamek.exe" "build/kamek.o" %OBJECTS% %debug% -dynamic -externals="%GAMESOURCE%/symbols.txt" -versions="%GAMESOURCE%/versions.txt" -output-combined=build\Code.pul
 
-if %ErrorLevel% equ 0 (
-    xcopy /Y build\*.pul "%RIIVO%\Binaries" >nul
-    xcopy /Y build\*.pul "%CREATOR%" >nul
+if %ErrorLevel% equ 0 if NOT "!RIIVO!" == "" (
+    xcopy /Y build\*.pul "%RIIVO%\Binaries" /i /q
     echo Binaries copied
 )
 
